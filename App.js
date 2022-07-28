5047a1d108fe562f079ae842571cb8c11cb7174a
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-07-28 09:38:59 +0200

    scheduled a local notification

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+import * as Notifications from 'expo-notifications';
 
 export default function App() {
+
+  const scheduleNotificationHandler = () => {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'My first local notification',
+        body: 'This is the body of the notification',
+        data: { userName: 'Max'},
+      },
+      trigger: {
+        // when notif should be delivered
+        seconds: 5,
+      },
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button  title='Schedule notification' onPress={scheduleNotificationHandler} />
       <StatusBar style="auto" />
     </View>
   );
